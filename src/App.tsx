9943094d56757a3f968117daa92908cb1f0cb1b8
Catherine Thompson <cathythompson0915@gmail.com>
2019-06-25 22:27:15 -0600

    add github link

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Button from '@material-ui/core/Button';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Button from '@material-ui/core/Button'
+import { ThemeProvider } from '@material-ui/styles'
+import { orange } from '@material-ui/core/colors'
+import {
+  createMuiTheme,
+  makeStyles,
+  createStyles,
+  Theme as AugmentedTheme
+} from '@material-ui/core/styles'
+import GithubIcon from './svg-icons/GithubIcon'
+import Link from '@material-ui/core/Link'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#870d4c'
+    },
+    secondary: {
+      main: '#01579b'
+    }
+  }
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Button variant="contained" color="primary">
-          Hello World
+    <ThemeProvider theme={theme}>
+      {/* <img src={logo} className='App-logo' alt='logo' /> */}
+      <Link href='https://github.com/CatherineThompson/home' target='_blank'>
+        <Button color='primary' >
+          <GithubIcon style={{ marginRight: 8 }} />
+           View Source Code
         </Button>
-      </header>
-    </div>
-  );
+      </Link>
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
